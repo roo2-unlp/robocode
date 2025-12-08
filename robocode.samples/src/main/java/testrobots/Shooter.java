dfa5acb7ef3717ff73634f19251b089acbacd1ed
Leonardo Delmas <DelmasLeonardo@gmail.com>
2025-12-08 00:11:24 -0300

    Se creo un Robot Shooter para testear
    - Se creo un Robot Shooter que reemplaza al robot RadioactiveRobot por temas de simplicidad
    - El Robot Shooter dispara de forma alternada bullets normales y bullets radioactivas
    
    Notas:
    - El robot RadioactiveRobot se elimino para que su nombre no preste a confusión.

---
+++ robocode.samples/src/main/java/testrobots/Shooter.java
@@ -0,0 +1,44 @@
+package testrobots;
+
+import java.awt.Color;
+
+import robocode.ScannedRobotEvent;
+
+/**
+ * Shooter - un robot que usaremos para implementar las balas radioactivas.
+ * <p>
+ * Su proposito es disparar balas normales y radiactivas alternando.
+ *
+ * @author Leo Delmas
+ */
+
+public class Shooter extends robocode.RadioactiveRobot {
+	boolean shootNormalBullet = true;
+
+	public void run() {
+
+		setBodyColor(Color.green);
+		setGunColor(Color.green);
+		setRadarColor(Color.green);
+		setScanColor(Color.green);
+		setBulletColor(Color.green);
+
+		while (true) {
+			turnGunRight(5);
+		}
+	}
+
+	public void onScannedRobot(ScannedRobotEvent e) {
+		if (getGunHeat() == 0) {
+			if (shootNormalBullet) {
+				fire(3);
+				out.println("shot Normal Bullet");
+			} else {
+				fireRadioactiveBullet(3);
+				out.println("shot Radioactive Bullet");
+			}
+			shootNormalBullet = !shootNormalBullet;
+			scan();
+		}
+	}
+}
