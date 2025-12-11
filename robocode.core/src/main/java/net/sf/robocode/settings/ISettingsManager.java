b2b71d16ce6dce6abedf67ab58f8fc62f617310f
Leonardo Delmas <DelmasLeonardo@gmail.com>
2025-12-11 00:01:17 -0300

    Se arreglo inconsistencia con “Visible Scan Arcs”
    - Se corrigió un bug donde, al activar "Visible Scan Bullets", cerrar Robocode y volver a abrirlo, ademas de "Visible Scan Bullets" aparecía marcada tambien “Visible Scan Arcs”.

--- robocode.core/src/main/java/net/sf/robocode/settings/ISettingsManager.java
+++ robocode.core/src/main/java/net/sf/robocode/settings/ISettingsManager.java
@@ -251,7 +251,7 @@ public interface ISettingsManager {
 	String
 			OPTIONS_VIEW_ROBOTNAMES = "robocode.options.view.robotNames",
 			OPTIONS_VIEW_SCANARCS = "robocode.options.view.scanArcs",
-			OPTIONS_VIEW_SCANBULLETS = "robocode.options.view.scanArcs",
+			OPTIONS_VIEW_SCANBULLETS = "robocode.options.view.scanBullets",
 			OPTIONS_VIEW_ROBOTENERGY = "robocode.options.view.robotEnergy",
 			OPTIONS_VIEW_GROUND = "robocode.options.view.ground",
 			OPTIONS_VIEW_TPS = "robocode.options.view.TPS",
