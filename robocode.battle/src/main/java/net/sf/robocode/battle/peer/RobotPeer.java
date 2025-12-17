602d69f08c1b3f9ec5e6f2d5834c782f3559d31f
Augusto Boschiazzo Marinelli <augus2608@gmail.com>
2025-12-17 20:14:15 -0300

    Saco comentario en RobotPeer

--- robocode.battle/src/main/java/net/sf/robocode/battle/peer/RobotPeer.java
+++ robocode.battle/src/main/java/net/sf/robocode/battle/peer/RobotPeer.java
@@ -1142,7 +1142,7 @@ public final class RobotPeer implements IRobotPeerBattle, IRobotPeer {
 				println("Daño extra: " + extraWallDamage);
 			// Update energy, but do not reset inactiveTurnCount
 			if (statics.isAdvancedRobot()) {
-				setEnergy(energy - Rules.getWallHitDamage(velocity, extraWallDamage), false); // enviar parametro distinto
+				setEnergy(energy - Rules.getWallHitDamage(velocity, extraWallDamage), false);
 			}
 
 			updateBoundingBox();
